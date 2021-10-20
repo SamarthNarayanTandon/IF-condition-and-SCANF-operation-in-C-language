61cee69789fb438a9977ab90ae501da9c26f4360
Samarth Narayan Tandon <91974569+SamarthNarayanTandon@users.noreply.github.com>
2021-10-20 14:41:17 +0530

    Add files via upload

---
+++ IF condition and scanf function. 08 Oct 20.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+// An example of (if) condition 
+main()
+{int x = 2, y=3,z ,a;
+z=x+y;
+printf("Welcome");
+printf("\npress zero  _");
+scanf("%d",&a);
+if(a==0)
+{printf("x+y=%d",z);}
+else
+{printf("error");}
+return 0;
+
+}
